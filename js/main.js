262121a4dd8a3e0f25b6b3b6d21f14026e80b8c2
Joy Xie <a556987@gmail.com>
2020-04-14 16:03:25 +0800

    update js

--- js/main.js
+++ js/main.js
@@ -3,16 +3,24 @@ let $title = $('#main .menu .title')
 
 function selectFolder(e) {
     let theFiles = e.target.files; // choose folder's all files
+    let images_array = [];
+
+    
 
     // have folder
     if (theFiles.length > 0) {
+        // resort
+        for (let i=0; i < theFiles.length ; i++) {
+            images_array.push(theFiles[i].webkitRelativePath);
+        }
+        images_array.sort();
+        
         // remove images
         $panel.empty();
 
         // append images
         for (let i=0; i < theFiles.length ; i++) {
-            let curRelativePath = theFiles[i].webkitRelativePath;
-            $panel.append(`<img src="./images/${curRelativePath}" />`);
+            $panel.append(`<img src="./images/${images_array[i]}" />`);
         }
 
         // update title
